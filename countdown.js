a862b915ac829a80de7e063ccc36362e30465af1
Alexander Skrabl <alexander@skrabl.de>
2015-05-19 23:42:31 +0100

    calculating stuff and some other bits

--- countdown.js
+++ countdown.js
@@ -9,31 +9,73 @@ class SimpleReactCountdown extends React.Component {
     constructor( props ) {
         super( props );
 
-        this.getCountDown();
-
         this.state = {
             hide: false,
             days: '00',
             hours: '00',
             mins: '00',
             secs: '00'
-        }
+        };
     }
 
-    getCountDown() {
+    componentDidMount() {
+        this.countdown();
+    }
+
+    addZero(val) {
+        if (val.toString().length === 1) {
+            val = '0' + val;
+        }
+        return val;
+    }
+
+    countdown() {
         var currTime = Date.now();
         var startDate = Date.parse(this.props.startDate);
         var endDate = Date.parse(this.props.endDate);
+        var timeLeft = endDate - currTime;
         var difference;
 
         if ( startDate < endDate ) {
             difference = endDate - startDate;
         }
         else {
+            this.setState({
+                hide: true
+            });
             throw new Error('Start date has to be smaller than end date.');
         }
 
+        if ( currTime > endDate && this.props.hideWhenFinished === true ) {
+            this.setState({
+                hide: true
+            });
+        }
 
+        if ( currTime < endDate ) {
+
+            var leftOverTime = {
+                days: Math.floor( timeLeft / (1000*60*60*24) ),
+                hours: Math.floor( (timeLeft / (1000*60*60)) % 24 ),
+                mins: Math.floor( (timeLeft / (1000*60)) % 60 ),
+                secs: Math.floor( (timeLeft / 1000 ) % 60 )
+            };
+
+            if ( this.props.showZero === true ) {
+                leftOverTime.days = this.addZero( leftOverTime.days );
+                leftOverTime.hours = this.addZero( leftOverTime.hours );
+                leftOverTime.mins = this.addZero( leftOverTime.mins );
+                leftOverTime.secs = this.addZero( leftOverTime.secs );
+            }
+
+            this.setState({
+                days: leftOverTime.days,
+                hours: leftOverTime.hours,
+                mins: leftOverTime.mins,
+                secs: leftOverTime.secs
+            });
+
+        }
 
         console.log(startDate, endDate, difference, currTime);
     }
