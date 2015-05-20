aea4363e4f8fdb2f7a2c127a7a8a0bf80074bb60
Alexander Skrabl <alexander@skrabl.de>
2015-05-20 20:38:56 +0100

    only passing one state now and calculating inside render

--- countdown.js
+++ countdown.js
@@ -11,10 +11,7 @@ class SimpleReactCountdown extends React.Component {
 
         this.state = {
             hide: false,
-            days: '00',
-            hours: '00',
-            mins: '00',
-            secs: '00'
+            timeLeft: 0
         };
 
         this.currTime = new Date().getTime();
@@ -64,25 +61,8 @@ class SimpleReactCountdown extends React.Component {
 
         if ( this.currTime < this.endDate ) {
 
-            var leftOverTime = {
-                days: Math.floor( this.timeLeft / (1000*60*60*24) ),
-                hours: Math.floor( (this.timeLeft / (1000*60*60)) % 24 ),
-                mins: Math.floor( (this.timeLeft / (1000*60)) % 60 ),
-                secs: Math.floor( (this.timeLeft / 1000 ) % 60 )
-            };
-
-            if ( this.props.showZero === true ) {
-                leftOverTime.days = this.addZero( leftOverTime.days );
-                leftOverTime.hours = this.addZero( leftOverTime.hours );
-                leftOverTime.mins = this.addZero( leftOverTime.mins );
-                leftOverTime.secs = this.addZero( leftOverTime.secs );
-            }
-
             this.setState({
-                days: leftOverTime.days,
-                hours: leftOverTime.hours,
-                mins: leftOverTime.mins,
-                secs: leftOverTime.secs
+                timeLeft: this.timeLeft
             });
 
             setTimeout(this.countdown.bind(this), 1000);
@@ -90,12 +70,38 @@ class SimpleReactCountdown extends React.Component {
     }
 
     render() {
+
+        var timeLeft = this.state.timeLeft;
+
+        var leftOverTime = {
+            days: 0,
+            hours: 0,
+            mins: 0,
+            secs: 0
+        };
+
+        if (timeLeft !== 0) {
+            leftOverTime = {
+                days: Math.floor( timeLeft / (1000*60*60*24) ),
+                hours: Math.floor( (timeLeft / (1000*60*60)) % 24 ),
+                mins: Math.floor( (timeLeft / (1000*60)) % 60 ),
+                secs: Math.floor( (timeLeft / 1000 ) % 60 )
+            };
+
+            if ( this.props.showZero === true ) {
+                leftOverTime.days = addZero( leftOverTime.days );
+                leftOverTime.hours = addZero( leftOverTime.hours );
+                leftOverTime.mins = addZero( leftOverTime.mins );
+                leftOverTime.secs = addZero( leftOverTime.secs );
+            }
+        }
+
         return (
             <div>
-                <span className="days">{ this.state.days }</span>
-                <span className="hours">{ this.state.hours }</span>
-                <span className="mins">{ this.state.mins }</span>
-                <span className="secs">{ this.state.secs }</span>
+                <span className="days">{ leftOverTime.days }</span>
+                <span className="hours">{ leftOverTime.hours }</span>
+                <span className="mins">{ leftOverTime.mins }</span>
+                <span className="secs">{ leftOverTime.secs }</span>
             </div>
         );
     }
